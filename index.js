ca698190b77efcaf6bb922cc4d19642eeb2fd45a
allanmauko <allan.mauko@student.moringaschool.com>
2022-06-22 22:33:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i < word.length /2 ; i++){
+
+    if(word[i] ===word[word.length - 1 - i]){
+      return true
+    }
+    else{
+      return false
+    }
+  }
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
